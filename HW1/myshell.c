e49d028dcb3a6d02466673f787a91f9e72546113
swalha1999 <mohamd514.m@gmail.com>
2024-05-30 22:53:52 +0300

    impl commadHistory double-linked-list to save the command

--- HW1/myshell.c
+++ HW1/myshell.c
@@ -7,12 +7,76 @@
 
 #define BUFFER_SIZE 100
 
+typedef struct Command* CommandPtr;
+typedef struct Command
+{
+    char *name;
+    char **args;
+    CommandPtr next_node;    
+    CommandPtr prev_node;
+} Command;
+
+typedef struct CommandHistory* CommandHistoryPtr;
+typedef struct CommandHistory
+{
+    CommandPtr head;
+    CommandPtr tail;
+} CommandHistory;
+
+CommandPtr createCommand(char *name, char **args)
+{
+    CommandPtr newCommand = (CommandPtr)malloc(sizeof(Command));
+    newCommand->name = name;
+    newCommand->args = args;
+    newCommand->next_node = NULL;
+    newCommand->prev_node = NULL;
+    return newCommand;
+}
+
+/* add a commad to end of the list */
+void addCommandToHistory(CommandHistoryPtr commandsHistory, CommandPtr newCommand)
+{
+    if(commandsHistory->head == NULL)
+    {
+        commandsHistory->head = newCommand;
+        commandsHistory->tail = newCommand;
+        return;
+    }
+ 
+    commandsHistory->tail->next_node = newCommand;
+    newCommand->prev_node = commandsHistory->tail;
+    commandsHistory->tail = newCommand;
+    
+}
+
+void printCommandHistory(CommandHistoryPtr commandHistory)
+{
+    CommandPtr temp = commandHistory->head;
+    while(temp != NULL)
+    {
+        fprintf(stdout, "%s\n", temp->name);
+        temp = temp->next_node;
+    }
+}
+
+void freeCommandHistory(CommandHistoryPtr commandHistory)
+{
+    CommandPtr temp = commandHistory->head;
+    CommandPtr next;
+    while(temp != NULL)
+    {
+        next = temp->next_node;
+        free(temp);
+        temp = next;
+    }
+}
+
 int main(void)
 {
     close(2);
     dup(1);
     char command[BUFFER_SIZE];
-    
+    CommandHistory commandHistory;
     while (1)
     {
         fprintf(stdout, "my-shell> ");
@@ -23,16 +87,16 @@ int main(void)
             break;
         }
 
-        /*
+        /* code starts here*/
+        addCommandToHistory(&commandHistory, createCommand("ls", NULL));
+        addCommandToHistory(&commandHistory, createCommand("cd", NULL));
+        addCommandToHistory(&commandHistory, createCommand("pwd", NULL));
 
-        ^
-        |
-            Add your code here
-        |
-        v
-
-        */
+        printCommandHistory(&commandHistory);
+        
+        /* code ends here*/
     }
 
+    freeCommandHistory(&commandHistory);
     return 0;
 }
