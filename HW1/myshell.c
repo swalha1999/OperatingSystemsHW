ed0ee29afda5b49657d60810372dee6af3d1a384
swalha1999 <mohamd514.m@gmail.com>
2024-06-02 23:52:21 +0300

    moved some files

--- HW1/myshell.c
+++ HW1/myshell.c
@@ -26,12 +26,22 @@ CommandPtr createCommand(char* rawCommand);
 void addCommandToHistory(CommandHistoryPtr commandsHistory, CommandPtr newCommand);
 void printCommandHistory(CommandHistoryPtr commandHistory);
 void freeCommandHistory(CommandHistoryPtr commandHistory);
+void executeCommand(char* command, char** args, int argsc);
+void executeCommandAsync(char* command);
+void executeCommandSync(char* command);
+void parseCommand(char* command, char* out_command, char** out_args, int* argsc);
+
+char* TrimStr(char* str);
+
 
 int main(void)
 {
     close(2);
     dup(1);
     char command[BUFFER_SIZE];
+    char out_command[BUFFER_SIZE];
+    char* out_args[BUFFER_SIZE];
+    int argsc;
     CommandHistory commandHistory = {NULL, NULL};
     while (1)
     {
@@ -43,24 +53,79 @@ int main(void)
             break;
         }
 
-        /* code starts here*/
-        addCommandToHistory(&commandHistory, createCommand("ls"));
-        addCommandToHistory(&commandHistory, createCommand("cd"));
-        addCommandToHistory(&commandHistory, createCommand("pwd"));
+        if(strncmp(command, "history", 7) == 0)
+        {
+            printCommandHistory(&commandHistory);
+            addCommandToHistory(&commandHistory, createCommand(command));
+            continue;
+        }
 
-        printCommandHistory(&commandHistory);
         
-        /* code ends here*/
+        addCommandToHistory(&commandHistory, createCommand(command));
+
+        
     }
 
     freeCommandHistory(&commandHistory);
     return 0;
 }
 
+void executeCommand(char* command, char** args, int argsc)
+{
+    if(args[argsc] == "&")
+    {   
+        executeCommandAsync(command);
+    }
+    else
+    {
+        executeCommandSync(command);
+    }
+}
+
+void executeCommandAsync(char* command)
+{
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("fork failed\n");
+        exit(1);
+    }
+    else
+    if(pid == 0)
+    {
+        char* args[] = {"/bin/sh", "-c", command, NULL};
+        execvp(args[0], args);
+        perror("error");
+        exit(0);
+    }
+    else
+    {
+        waitpid(pid, NULL, 0);
+    }
+}
+
+void executeCommandSync(char* command)
+{
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        printf("fork failed\n");
+        exit(1);
+    }
+    if (pid == 0)
+    {
+        char* args[] = {"/bin/sh", "-c", command, NULL};
+        execvp(args[0], args);
+        perror("error");
+        exit(0);
+    }
+}
+
 CommandPtr createCommand(char* rawCommand)
 {
     CommandPtr newCommand = (CommandPtr)malloc(sizeof(Command));
-    newCommand->rawCommamd = rawCommand;
+    newCommand->rawCommamd = (char*)malloc(BUFFER_SIZE);
+    strcpy(newCommand->rawCommamd, rawCommand);
     newCommand->next_node = NULL;
     newCommand->prev_node = NULL;
     return newCommand;
@@ -99,7 +164,23 @@ void freeCommandHistory(CommandHistoryPtr commandHistory)
     while(temp != NULL)
     {
         next = temp->next_node;
+        free(temp->rawCommamd);
         free(temp);
         temp = next;
     }
+}
+
+
+void parseCommand(char* command, char* out_command, char** out_args, int* argsc){
+    char* token = strtok(command, " ");
+    strcpy(out_command, token);
+    int i = 0;
+    while(token != NULL)
+    {
+        out_args[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+    out_args[i] = NULL;
+    *argsc = i;
 }
